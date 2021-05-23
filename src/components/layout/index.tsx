cf089cf5b74cca93d6b02f70e456711a81ae5472
mata <muhammad_ata@outlook.com>
2021-05-23 04:20:57 +0300

    improved ui of steppers

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,13 +1,91 @@
 import * as React from 'react';
-import { Container, Grid, Stepper, Step, StepLabel, Button } from '@material-ui/core';
+import { Container, Grid, Stepper, Step, StepLabel, Button, StepConnector, StepIconProps } from '@material-ui/core';
+import { makeStyles, Theme, createStyles, withStyles } from '@material-ui/core/styles';
 import Steps from '../steps'
 import appReducer from '../../store/reducers';
 import AppContext from '../../store/context';
 import { initialState } from '../../store/constants';
 import Ticket from '../ticket';
 import { useStyles } from './style';
+import clsx from 'clsx'
+import AccessibilityIcon from '@material-ui/icons/Accessibility';
+import CategoryIcon from '@material-ui/icons/Category';;
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import AlarmIcon from '@material-ui/icons/Alarm';
+import DirectionsBusIcon from '@material-ui/icons/DirectionsBus';
 
 const { useState } = React
+  
+  const ColorlibConnector = withStyles({
+    alternativeLabel: {
+      top: 22,
+    },
+    active: {
+      '& $line': {
+        backgroundImage:
+          'linear-gradient( 95deg,rgb(63, 81, 181) 0%,rgb(87, 104, 201) 50%,rgb(117, 128, 191) 100%)',
+      },
+    },
+    completed: {
+      '& $line': {
+        backgroundImage:
+          'linear-gradient( 95deg,rgb(63, 81, 181) 0%,rgb(63, 81, 181) 50%,rgb(63, 81, 181) 100%)',
+      },
+    },
+    line: {
+      height: 3,
+      border: 0,
+      backgroundColor: '#eaeaf0',
+      borderRadius: 1,
+    },
+  })(StepConnector);
+  
+  const useColorlibStepIconStyles = makeStyles({
+    root: {
+      backgroundColor: '#ccc',
+      zIndex: 1,
+      color: '#fff',
+      width: 50,
+      height: 50,
+      display: 'flex',
+      borderRadius: '50%',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    active: {
+      backgroundImage:
+        'linear-gradient( 136deg, rgb(63, 81, 181) 0%, rgb(87, 104, 201) 50%, rgb(117, 128, 191) 100%)',
+      boxShadow: '0 4px 10px 0 rgba(0,0,0,.25)',
+    },
+    completed: {
+      backgroundImage:
+        'linear-gradient( 136deg, rgb(63, 81, 181) 0%, rgb(63, 81, 181) 50%, rgb(63, 81, 181) 100%)',
+    },
+  });
+
+  function ColorlibStepIcon(props: StepIconProps) {
+    const classes = useColorlibStepIconStyles();
+    const { active, completed } = props;
+  
+    const icons: { [index: string]: React.ReactElement } = {
+      1: <AccessibilityIcon />,
+      2: <CategoryIcon />,
+      3: <LocationOnIcon />,
+      4: <AlarmIcon />,
+      5: <DirectionsBusIcon />
+    };
+  
+    return (
+      <div
+        className={clsx(classes.root, {
+          [classes.active]: active,
+          [classes.completed]: completed,
+        })}
+      >
+        {icons[String(props.icon)]}
+      </div>
+    );
+  }
 
 function getSteps() {
     return ['Name', 'Type', 'Where to move', 'When to move', 'Select Ticket'];
@@ -64,10 +142,10 @@ const Layout = () => {
             <Container className={classes.layout}>
                 <Grid container >
                     <Grid item xs={12}>
-                        <Stepper alternativeLabel activeStep={activeStep}>
+                        <Stepper connector={<ColorlibConnector />} alternativeLabel activeStep={activeStep}>
                             {steps.map((label) => (
                                 <Step key={label}>
-                                    <StepLabel>{label}</StepLabel>
+                                    <StepLabel StepIconComponent={ColorlibStepIcon}>{label}</StepLabel>
                                 </Step>
                             ))}
                         </Stepper>
